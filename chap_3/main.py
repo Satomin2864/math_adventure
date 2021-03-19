ad1b97d53f4d3db068d73729e0a1a02bbc23e5fd
Satomin2864 <satomiyuta0602@gmail.com>
2021-03-19 22:35:40 +0900

    chap3完了

--- chap_3/main.py
+++ chap_3/main.py
@@ -7,5 +7,28 @@ def gcf(num_1, num_2):
         if num_1 % i == 0 and num_2 % i == 0:
             gcd = i
     return gcd
-    
+
+# 3-2
+def average(a, b):
+    return (a + b) / 2
+
+def square_root(num, low, high):
+    for i in range(20):
+        guess = average(low, high)
+        # print(guess)
+        if guess ** 2 == num:
+            break
+        elif guess ** 2 > num:  # 小さいと予想する
+            high = guess
+        else:  # 大きいと予想する
+            low = guess
+    print(guess)
+
 print(gcf(150,138))
+
+# 200   14と15の間
+square_root(200, 14, 15)
+# 1000  31と32の間
+square_root(1000, 31, 32)
+# 50000 31と32の間
+square_root(50000, 223, 224)
